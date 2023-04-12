b51c3c15ee30e661dad8d7e1ebd4cf1ffc6c3f15
fkastner <kastner.felix@gmail.com>
2023-04-12 20:03:56 +0000

    Deploying to gh-pages from  @ 61703e0e7dd47cb48ff7ee37e1be71700699af19 🚀

---
+++ libs/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  // Follow menu drop down
+  $(".author__urls-wrapper button").on("click", function() {
+    $(".author__urls").toggleClass("is--visible");
+    $(".author__urls-wrapper button").toggleClass("open");
+  });
+});
